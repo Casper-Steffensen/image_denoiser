8ef90c551965c022f49be7a9c29dd143a1a13764
paulseghers <0099seghers@gmail.com>
2022-02-02 04:59:46 +0100

    fixed bugs and pep8 compliance

--- README.md
+++ README.md
@@ -3,8 +3,7 @@ This project is for removing noise from scans of technical documents.
 
 ### List of supported formats
 
-    - .tiff
-    - .jpegerino
+currently [all formats supported by the PILLOW library](https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html) in version 9.0.0 should be supported. some of the weird ones with exotic (e.g. with many bits per channel) probably aren't and you will find this out if PIL gives you spicy errors
 
 ### requirements
 please run `pipreqs`
@@ -14,4 +13,5 @@ please run `pipreqs`
 ```
 python src/denoiser.py
 ```
-then let the terminal guide you
+provided the image `test_pic.jpeg` is in your `inputs/` folder.<br>
+this will create a cleaned output you'll be able to find at `outputs/test_pic_denoised.jpeg`
--- src/denoiser.py
+++ src/denoiser.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 import os
+
 # from queue import Queue
 # from threading import Thread
 # from time import time
@@ -8,131 +9,133 @@ import os
 # Two thresholds are defined (black and white) and pixels are changed
 # To either 100% white or relativly close to black
 
-def filter_threshold(img_array):
-	"""
-	Her defineres funktionen som fjerner salt og peber støj
-	input: 2D np array af billedet
-	returnerer: ny np.array af billedet med mindre "støj"
-	"""
-	size = img_array.shape
-	for x in range(size[0]):
-		for y in range(size[1]):
-			if img_array[x, y] <= 50:
-				img_array[x, y] = 0
 
-			elif img_array[x, y] >= 150:
-				img_array[x, y] = 255
-	print("Threshold filtering done")			
-	return img_array
+def filter_threshold(img_array):
+    """
+    Her defineres funktionen som fjerner salt og peber støj
+    input: 2D np array af billedet
+    returnerer: ny np.array af billedet med mindre "støj"
+    """
+    size = img_array.shape
+    for x in range(size[0]):
+        for y in range(size[1]):
+            if img_array[x, y] <= 50:
+                img_array[x, y] = 0
+
+            elif img_array[x, y] >= 150:
+                img_array[x, y] = 255
+    print("Threshold filtering done")
+    return img_array
 
 
 def closest_multiple(num_lines, p):
-	if(num_lines % p):# := if(0) == if(False)
-		return x-(x%p) #num_lines?
-	else:
-		return num_lines
+    if num_lines % p:  # := if(0) == if(False)
+        return num_lines - (num_lines % p)  # num_lines?
+    else:
+        return num_lines
 
 
 def filter_threshold_pll(img_array, num_cores):
-	for x in range(size[0]):
-		for y in range(size[1]):
-			if img_array[x, y] <= 50:
-				img_array[x, y] = 0
+    for x in range(size[0]):
+        for y in range(size[1]):
+            if img_array[x, y] <= 50:
+                img_array[x, y] = 0
 
-			elif img_array[x, y] >= 150:
-				img_array[x, y] = 255
+            elif img_array[x, y] >= 150:
+                img_array[x, y] = 255
 
-	print("Threshold filtering done")			
-	return img_array
+    print("Threshold filtering done")
+    return img_array
 
 
 def remove_isolated_pixels(img_array):
-	size = img_array.shape
-	"""
+    size = img_array.shape
+    """
 	loop where we define how fine we want the filtering
 	"""
-	pixels_turned_on = 0
+    pixels_turned_on = 0
 
-	for i in range(size[0]-3):
-		for j in range(size[1]-3): #3 er arbitrær, så vi ikke overskrider
-			if img_array[i,j] != 255:
-				pixels_turned_on +=1
+    for i in range(size[0] - 3):
+        for j in range(size[1] - 3):  # 3 er arbitrær, så vi ikke overskrider
+            if img_array[i, j] != 255:
+                pixels_turned_on += 1
 
-				#Looking 2 pixels up on y-axis
-				for pixels in range(2):
-					if img_array[i-pixels,j] != 255:
-						pixels_turned_on +=1
+                # Looking 2 pixels up on y-axis
+                for pixels in range(2):
+                    if img_array[i - pixels, j] != 255:
+                        pixels_turned_on += 1
 
-				#Looking 3 pixels down on y-axis	
-				for pixels in range(2):
-					if img_array[i+pixels,j] != 255:
-						pixels_turned_on +=1
+                # Looking 3 pixels down on y-axis
+                for pixels in range(2):
+                    if img_array[i + pixels, j] != 255:
+                        pixels_turned_on += 1
 
-				#Looking 3 pixels right on x-axis			
-				for pixels in range(2):
-					if img_array[i,j+pixels] !=255:
-						pixels_turned_on +=1
-				if pixels_turned_on <= 4:
-					img_array[i,j] = 255		
-			pixels_turned_on = 0
-	print("Single pixels removed")		
-	return img_array 				
+                # Looking 3 pixels right on x-axis
+                for pixels in range(2):
+                    if img_array[i, j + pixels] != 255:
+                        pixels_turned_on += 1
+                if pixels_turned_on <= 4:
+                    img_array[i, j] = 255
+            pixels_turned_on = 0
+    print("Single pixels removed")
+    return img_array
 
 
 def remove_2x1_pixels(img_array):
-	size = img_array.shape
+    size = img_array.shape
 
-	"""
+    """
 	Where we remove 2x1 pixel objects
 	"""
-	white_pixels = 0
+    white_pixels = 0
 
-	for i in range(size[0]-2):
-		for j in range(size[1]-2): #2 er arbitrær, så vi ikke overskrider
-			if img_array[i,j] != 255:
+    for i in range(size[0] - 2):
+        for j in range(size[1] - 2):  # 2 er arbitrær, så vi ikke overskrider
+            if img_array[i, j] != 255:
 
-				#Looking if pixel above is at value 255
-				if img_array[i-1,j] == 255:
-					white_pixels += 1
+                # Looking if pixel above is at value 255
+                if img_array[i - 1, j] == 255:
+                    white_pixels += 1
 
-				#Looking if pixel to the right is at value 255
-				if img_array[i,j+1] == 255:
-					white_pixels += 1
+                # Looking if pixel to the right is at value 255
+                if img_array[i, j + 1] == 255:
+                    white_pixels += 1
 
-				#Looking if the 2nd pixel below is at value 255
-				if img_array[i+2,j] == 255:
-					white_pixels += 1
+                # Looking if the 2nd pixel below is at value 255
+                if img_array[i + 2, j] == 255:
+                    white_pixels += 1
 
-				#Looking if pixel the the left is at value 255
-				if img_array[i,j-1] == 255:
-					white_pixels += 1
+                # Looking if pixel the the left is at value 255
+                if img_array[i, j - 1] == 255:
+                    white_pixels += 1
 
-				if white_pixels == 4:
-					img_array[i,j] = 255
-					img_array[i+1,j] = 255
+                if white_pixels == 4:
+                    img_array[i, j] = 255
+                    img_array[i + 1, j] = 255
 
-			white_pixels = 0
-	print("2x1 blobs removed")		
-	return img_array 			
+            white_pixels = 0
+    print("2x1 blobs removed")
+    return img_array
+
 
 ###############################################
 #    Functions defined: ok || main loop:      #
 ###############################################
 
-img = Image.open("test_pic.tiff").convert("L")
+img = Image.open(os.getcwd()+"/inputs/test_pic.jpeg").convert("L")
 img_array = np.array(img)
 
-print(img_array.shape)
+print("got shape: ", img_array.shape)
 
 img_array_th_fltrd = filter_threshold(img_array)
 
-for _ in range(2):
+for _ in range(2):  # why 2?
 
-	img_array_final = remove_isolated_pixels(img_array_th_fltrd)
+    img_array_final = remove_isolated_pixels(img_array_th_fltrd)
 
 two_by_one_pixels = remove_2x1_pixels(img_array_final)
 
 print("Done!")
 
 im = Image.fromarray(img_array)
-im.save("ouputs/denoised.tiff")
+im.save(os.getcwd()+"/ouputs/denoised_test.jpeg")
